ec56196ab71b2689433eeab774a34edc25b33642
Nil Silva <Nil.Silva@pt.softinsa.com>
2021-06-23 13:30:11 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,59 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'testCarbonSlider';
+  value = 0;
+  items = [
+    {
+      id: 1,
+      name: 'One',
+      experienceLevel: 45,
+    },
+    {
+      id: 2,
+      name: 'Two',
+      experienceLevel: 4,
+    },
+    {
+      id: 3,
+      name: 'Three',
+      experienceLevel: 86,
+    },
+    {
+      id: 4,
+      name: 'Four',
+      experienceLevel: 34,
+    },
+    {
+      id: 5,
+      name: 'Five',
+      experienceLevel: 98,
+    },
+    {
+      id: 6,
+      name: 'Six',
+      experienceLevel: 1,
+    },
+  ];
+
+  valueChange(event: any, id: number) {
+    setTimeout(() => {
+      if (id) {
+        let index = this.items.findIndex((skill) => {
+          return skill.id === id;
+        });
+
+        let value = this.items[index].experienceLevel;
+        if (event !== value) {
+          this.items[index].experienceLevel = event;
+        }
+      }
+    }, 0);
+  }
+
+  printArray() {
+    console.log(this.items);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AccordionModule, SliderModule } from 'carbon-components-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SliderModule, AccordionModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
